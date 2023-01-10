32aa367e7aa57a4e6547823898118ec99ced9263
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-09 22:34:51 -0300

    bugs concertados

--- script/index.js
+++ script/index.js
@@ -7,47 +7,51 @@ function verificar()
     var idade = Number(2023) - Number(anoNasc.value)
     var digitou = false
 
-    if (document.getElementById("inasc") == typeof(Number)){
-
-    if(document.getElementById("imasc").checked){
-        if(idade > 0 && idade < 13){
-            imagem.src = "imagens/criancaMasc.jpg"
-            document.getElementById("container").style.height = "78vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
-        }
-        if(idade > 12 && idade < 18){
-            imagem.src = "imagens/adolMasc.jpg"
-            document.getElementById("container").style.height = "75vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
-        }
-        if(idade > 17){
-            imagem.src = "imagens/adultoMasc.jpg"
-            document.getElementById("container").style.height = "80vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
-        }
+    if (anoNasc.value.length == 0)
+    {
+        alert("Digite sua idade")
     }
-
     else
     {
-        if(idade < 12){
-            imagem.src = "imagens/criancaFem.jpg"
-            document.getElementById("container").style.height = "75vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+        if(document.getElementById("imasc").checked)
+        {
+            if(idade > 0 && idade < 13){
+                imagem.src = "imagens/criancaMasc.jpg"
+                document.getElementById("container").style.height = "78vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
+            if(idade > 12 && idade < 18){
+                imagem.src = "imagens/adolMasc.jpg"
+                document.getElementById("container").style.height = "75vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
+            if(idade > 17){
+                imagem.src = "imagens/adultoMasc.jpg"
+                document.getElementById("container").style.height = "80vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
         }
-        if(idade > 12 && idade < 18){
-            imagem.src = "imagens/adolFem.jpg"
-            document.getElementById("container").style.height = "78vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
-        }
-        if(idade > 17){
-            imagem.src = "imagens/adultaFem.jpg"
-            document.getElementById("container").style.height = "80vh"
-            imagem.style.height = "55vh"
-            document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+
+        else
+        {
+            if(idade < 12){
+                imagem.src = "imagens/criancaFem.jpg"
+                document.getElementById("container").style.height = "75vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
+            if(idade > 12 && idade < 18){
+                imagem.src = "imagens/adolFem.jpg"
+                document.getElementById("container").style.height = "78vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
+            if(idade > 17){
+                imagem.src = "imagens/adultaFem.jpg"
+                document.getElementById("container").style.height = "80vh"
+                imagem.style.height = "55vh"
+                document.getElementById("iRes").innerText = `Você tem ${idade} anos de idade`
+            }
         }
     }
-}
-else{
-    alert("Você deve digitar um numero")
-}
+
+
 }
